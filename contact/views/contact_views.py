2979317ebf7b2e4a2d0b846fc87a48f1ccfcb810
João Victor <joaovictorsalotialves12@gmail.com>
2023-05-03 09:12:25 -0300

    creating partial files for header and head

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -10,6 +10,7 @@ def index(request):
 
     context = {
         'contacts': contats,
+        'site_title': 'Contatos - ',
     }
 
     return render(
@@ -23,9 +24,11 @@ def contact(request, contact_id):
     single_contact = get_object_or_404(
         Contact, pk=contact_id, show=True
     )
+    contact_name = f'{single_contact.first_name} {single_contact.last_name} - '
 
     context = {
         'contact': single_contact,
+        'site_title': contact_name,
     }
 
     return render(
