efd704237892d7e2e0ec0b680d987ca2cf6f7f4f
João Victor <joaovictorsalotialves12@gmail.com>
2023-05-03 07:59:07 -0300

    creating contact table in contact/index.html

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from contact.models import Contact
 
 
 def index(request):
+    contats = Contact.objects.all()
+
+    context = {
+        'contacts': contats,
+    }
+
     return render(
         request,
         'contact/index.html',
+        context,
     )
