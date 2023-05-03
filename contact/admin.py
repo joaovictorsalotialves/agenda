808cc79ad3fd603e7f0de759b65e46b7877d1942
João Victor <joaovictorsalotialves12@gmail.com>
2023-05-03 11:15:11 -0300

    creating pagination

--- contact/admin.py
+++ contact/admin.py
@@ -8,8 +8,8 @@ class ContactAdmin(admin.ModelAdmin):
     ordering = '-id',
     # list_filter = 'created_date'
     search_fields = 'id', 'first_name', 'last_name',
-    list_per_page = 100
-    list_max_show_all = 200
+    list_per_page = 10
+    list_max_show_all = 10
     list_editable = 'first_name', 'last_name', 'show',
     list_display_links = 'id', 'phone',
 
